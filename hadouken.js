9ac1ec41aa41539912f9401d8eb550a57771dc9a
Breno Santos <brenosantos145@gmail.com>
2023-10-24 21:15:32 -0300

    hadooooooooooooouken

---
+++ hadouken.js
@@ -0,0 +1,23 @@
+function hadouken() {
+  if ("i") {
+    if ("L") {
+      if ("o") {
+        if ("v") {
+          if ("e") {
+            if ("t") {
+              if ("h") {
+                if ("i") {
+                  if ("s") {
+                    console.log("beautiful");
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+hadouken();
