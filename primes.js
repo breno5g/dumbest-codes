80f3c8aa0ba58bb6532582937b16b897b9aa8803
Breno Santos <brenosantos145@gmail.com>
2023-10-24 21:03:29 -0300

    The terror of encryption

---
+++ primes.js
@@ -0,0 +1,14 @@
+function isPrime(n) {
+  if (n === 1) return "Is not a prime";
+  if (n === 2) return "Is prime";
+  if (n === 3) return "Is prime";
+  if (n === 4) return "Is not a prime";
+  if (n === 5) return "Is prime";
+  if (n === 6) return "Is not a prime";
+  if (n === 7) return "Is prime";
+  if (n === 8) return "Is not a prime";
+  if (n === 9) return "Is not a prime";
+  if (n === 10) return "Is not a prime";
+}
+
+console.log(isPrime(3));
